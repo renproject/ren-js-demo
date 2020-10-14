1e06936433de726caf635470950a81a797e6d0c8
noiach <gh@noiach.com>
2020-10-14 20:45:40 +1100

    Updates

---
+++ precache-manifest.e0f509791f83a30473df1ff5c060e2ac.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5850e185943fc7727d1e93a299108c7",
+    "url": "/ren-js-demo/index.html"
+  },
+  {
+    "revision": "dd580f83079aa039f43b",
+    "url": "/ren-js-demo/static/css/main.29edaf36.chunk.css"
+  },
+  {
+    "revision": "219aa2618c176b802491",
+    "url": "/ren-js-demo/static/js/2.99cba183.chunk.js"
+  },
+  {
+    "revision": "60149c0d92d33573f3fe99690fe39112",
+    "url": "/ren-js-demo/static/js/2.99cba183.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dd580f83079aa039f43b",
+    "url": "/ren-js-demo/static/js/main.2715a87c.chunk.js"
+  },
+  {
+    "revision": "7e1e9a715ca117b3210f",
+    "url": "/ren-js-demo/static/js/runtime-main.eddcf83a.js"
+  },
+  {
+    "revision": "3fcae91e84ed28765082a1f603c19217",
+    "url": "/ren-js-demo/static/media/metamask.3fcae91e.svg"
+  }
+]);
